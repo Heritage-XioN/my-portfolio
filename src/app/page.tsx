e4862b6958a3c6414e3a9aba5ed3c2f396ab9fdd
HERITAGE-XION <iheritage934@gmail.com>
2025-11-14 11:36:44 +0100

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+	return (
+		<div className='  h-screen w-full'>
+			<div className=' h-screen w-full'>
+				
+			</div>
+		</div>
+	);
+}
---
+++ src/lib/linkpath.ts
@@ -0,0 +1,34 @@
+type paths = [{
+    url: string;
+    display: string;
+}]
+
+const linkPaths = [
+     {
+        url: "/",
+        display: "Home"
+    },
+    {
+        url: "/about",
+        display: "About"
+    },
+    {
+        url: "/services",
+        display: "Services"
+    },
+    {
+        url: "/portfolio",
+        display: "Portfolio"
+    },
+    {
+        url: "/journal",
+        display: "Journal"
+    },
+    {
+        url: "/contact",
+        display: "Contact"
+    }
+]
+
+
+export default linkPaths
